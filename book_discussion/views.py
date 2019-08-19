4ff9ba855fb91499230b00d0a87c77cd4c93e273
ZuuZaa <zumbaghirova@gmail.com>
2019-08-19 16:53:52 +0400

    discussion is ready

--- book_discussion/views.py
+++ book_discussion/views.py
@@ -3,7 +3,6 @@ from books.models import Book
 from .models import BookDiscussion
 from django.contrib.auth.decorators import login_required
 
-@login_required   
 def discussion(request, id):
     book = Book.objects.get(id=id)
     discussions = BookDiscussion.objects.filter(book=id).order_by('-date')
@@ -13,13 +12,16 @@ def discussion(request, id):
     }
 
     if request.method == 'POST':
-        comment_text = request.POST.get('comment')
-        new_comment = BookDiscussion(
-            user = request.user,
-            book = book,
-            comment = comment_text
-        )
-        new_comment.save()
-        return redirect('discussion', id)
+        if request.user.is_anonymous:
+            return redirect('login')
+        else:
+            comment_text = request.POST.get('comment')
+            new_comment = BookDiscussion(
+                user = request.user,
+                book = book,
+                comment = comment_text
+            )
+            new_comment.save()
+            return redirect('discussion', id)
 
     return render(request, 'discussion.html', context)
